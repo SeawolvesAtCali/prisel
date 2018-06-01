ee83d4906d6b5f791884c3340d92ed6e9387e5b1
Yinqian Zheng <YinqianZheng@Yinqians-MacBook-Pro.local>
2018-06-01 02:56:12 -0700

    close #15

--- client/controller/index.js
+++ client/controller/index.js
@@ -1,16 +1,15 @@
-/* eslint-disable */
 const io = require('socket.io-client');
-const readline = require("readline");
-const socket = io('http://localhost:3000');
+const readline = require('readline');
 const chatMessages = require('../message/chat');
-var rl = readline.createInterface(process.stdin, process.stdout);
-rl.on('line', function (line) {
+
+const socket = io('http://localhost:3000');
+const rl = readline.createInterface(process.stdin, process.stdout);
+rl.on('line', (line) => {
     socket.emit(...chatMessages.getMessage(line));
     rl.prompt(true);
 });
 
 // display what server send
-socket.on('broadcast', function(data){
+socket.on('broadcast', (data) => {
     console.log(data);
 });
-

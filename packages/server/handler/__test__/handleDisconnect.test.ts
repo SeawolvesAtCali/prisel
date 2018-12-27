87d8b94e543a6a24072430f18c3b9cee45dcf13f
Yiou Chen <chen.yiou1992@gmail.com>
2018-12-27 12:02:48 -0800

    #54 unit tests for handleLogin and handleDisconnect

---
+++ packages/server/handler/__test__/handleDisconnect.test.ts
@@ -0,0 +1,29 @@
+import { handleDisconnect } from '../handleDisconnect';
+import { Socket } from '../../objects';
+import createContext from '../../createContext';
+
+jest.mock('../../networkUtils');
+
+describe('handleDisconnect', () => {
+    it('should remove the client from socket and state', () => {
+        const mockClient = {} as Socket;
+        const mockContext = createContext({
+            StateManager: {
+                connections: {
+                    client1: {
+                        id: 'client1',
+                        username: 'user',
+                    },
+                },
+                messages: [],
+                rooms: {},
+            },
+        });
+        mockContext.SocketManager.add('client1', mockClient);
+        expect(mockContext.StateManager.connections.client1).toBeDefined();
+        expect(mockContext.SocketManager.hasSocket(mockClient)).toBe(true);
+        handleDisconnect(mockContext, mockClient)({});
+        expect(mockContext.StateManager.connections.client1).toBeUndefined();
+        expect(mockContext.SocketManager.hasSocket(mockClient)).toBe(false);
+    });
+});

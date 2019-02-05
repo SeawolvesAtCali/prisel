f37ec382934cdaab9f003cdb09c572229b0e50d3
yiochen <chen.yiou1992@gmail.com>
2019-02-04 23:17:48 -0500

    bind to 0.0.0.0 instead of localhost because https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error

--- packages/tic-tac-toe/index.ts
+++ packages/tic-tac-toe/index.ts
@@ -1,5 +1,5 @@
 import { Server } from '@prisel/server';
 import TicTacToe from './state';
 
-const server = new Server({ host: 'localhost', port: Number(process.env.PORT) || 3000 });
+const server = new Server({ host: '0.0.0.0', port: Number(process.env.PORT) || 3000 });
 server.register(TicTacToe);

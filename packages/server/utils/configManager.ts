68a4879ae22e0ce7b9f9d8c17c8ae814ef1280c0
Yiou Chen <chen.yiou1992@gmail.com>
2019-08-25 12:26:14 -0700

    Refactored configManager to allow more flexible game-room pairing

--- packages/server/utils/configManager.ts
+++ packages/server/utils/configManager.ts
@@ -2,41 +2,46 @@ import { GameConfig, BaseGameConfig } from './gameConfig';
 import { RoomConfig, BaseRoomConfig } from './roomConfig';
 
 class ConfigManager {
-    private gameConfigs: { [gameType: string]: GameConfig } = {
-        [BaseGameConfig.type]: BaseGameConfig,
-    };
-    private roomConfigs: { [roomType: string]: RoomConfig } = {
-        [BaseRoomConfig.type]: BaseRoomConfig,
-    };
-    private gameRoomPair: { [gameType: string]: Set<string> } = {
-        [BaseGameConfig.type]: new Set([BaseRoomConfig.type]),
-    };
+    private gameConfigs = new Map<GameConfig['type'], GameConfig>();
+    private roomConfigs = new Map<RoomConfig['type'], RoomConfig>();
 
     public add(gameConfig: GameConfig, roomConfig: RoomConfig) {
-        this.gameConfigs[gameConfig.type] = gameConfig;
-        this.roomConfigs[roomConfig.type] = roomConfig;
-        const allRoomTypesForGame = this.gameRoomPair[gameConfig.type] || new Set<string>();
-        allRoomTypesForGame.add(roomConfig.type);
-        this.gameRoomPair[gameConfig.type] = allRoomTypesForGame;
+        this.addGame(gameConfig);
+        this.addRoom(roomConfig);
+    }
+
+    public addGame(gameConfig: GameConfig) {
+        this.gameConfigs.set(gameConfig.type, gameConfig);
+    }
+
+    public addRoom(roomConfig: RoomConfig) {
+        this.roomConfigs.set(roomConfig.type, roomConfig);
     }
 
     public get(
         gameType: string = BaseGameConfig.type,
         roomType: string = BaseRoomConfig.type,
     ): { gameConfig: GameConfig; roomConfig: RoomConfig } {
-        const allRoomTypesForGame = this.gameRoomPair[gameType] || new Set<string>();
-        if (allRoomTypesForGame && allRoomTypesForGame.has(roomType)) {
+        const gameConfig = this.gameConfigs.get(gameType);
+        const roomConfig = this.roomConfigs.get(roomType);
+
+        if (roomConfig.supportGame(gameConfig)) {
             return {
-                gameConfig: this.gameConfigs[gameType],
-                roomConfig: this.roomConfigs[roomType],
+                gameConfig,
+                roomConfig,
             };
         }
     }
 
-    public getAllTypes() {
+    public getAllTypes(): {
+        gameTypes: Array<GameConfig['type']>;
+        roomTypes: Array<RoomConfig['type']>;
+    } {
+        const gameConfigs = Array.from(this.gameConfigs.keys());
+        const roomConfigs = Array.from(this.roomConfigs.keys());
         return {
-            gameTypes: Object.keys(this.gameConfigs),
-            roomTypes: Object.keys(this.roomConfigs),
+            gameTypes: gameConfigs.length > 0 ? gameConfigs : [BaseGameConfig.type],
+            roomTypes: roomConfigs.length > 0 ? roomConfigs : [BaseRoomConfig.type],
         };
     }
 }

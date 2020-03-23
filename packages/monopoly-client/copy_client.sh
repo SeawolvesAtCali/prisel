53eca23fa4a17d54a2fc45931d615a01848dc013
Yiou Chen <chen.yiou1992@gmail.com>
2020-03-22 23:26:42 -0700

    build priselClient using rollup. add priselClient meta to git tracking

--- packages/monopoly-client/copy_client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -p ./assets/Script
-cp ../client/lib/index.umd.js ./assets/Script/packages/priselClient.js
-
--- packages/monopoly-client/rollup.config.js
+++ packages/monopoly-client/rollup.config.js
@@ -1,6 +1,22 @@
 import dts from 'rollup-plugin-dts';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 const config = [
+    {
+        input: 'priselClient.js',
+        output: [
+            { file: 'assets/Script/packages/priselClient.js', format: 'umd', name: 'priselClient' },
+        ],
+        plugins: [
+            resolve({
+                // need to specify preferBuiltins
+                // https://github.com/rollup/rollup-plugin-node-resolve/issues/196
+                preferBuiltins: true,
+            }), // so Rollup can find dependencies
+            commonjs(), // so Rollup can convert dependencies to an ES module
+        ],
+    },
     {
         input: '../client/lib/index.d.ts',
         output: [

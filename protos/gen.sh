eb544859058a41ed0e97f39021c1c2b0540fd5c2
Yiou Chen <chen.yiou1992@gmail.com>
2020-10-11 23:53:09 -0700

    generate file to gen_ts folder. created barrel index.ts that reexports everything

--- protos/gen.sh
+++ protos/gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # https://github.com/stephenh/ts-proto
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. --ts_proto_opt=oneof=unions --experimental_allow_proto3_optional **/*.proto
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=gen_ts --ts_proto_opt=oneof=unions --experimental_allow_proto3_optional **/*.proto

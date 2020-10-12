65ea0c2c1d9f58c4b50e381e43792cc7df4e6f95
Yiou Chen <chen.yiou1992@gmail.com>
2020-10-11 17:11:08 -0700

    added ts-proto. Created proto file for @prisel/common

---
+++ protos/gen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# https://github.com/stephenh/ts-proto
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. --ts_proto_opt=oneof=unions --experimental_allow_proto3_optional **/*.proto

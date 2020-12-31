33222a1b834ddc506e0ca6565caebb76f1d2c783
Yiou Chen <chen.yiou1992@gmail.com>
2020-12-30 19:19:40 -0800

    add prisel/protos to monopoly-common

--- packages/monopoly-common/types/gamePlayerInfo.ts
+++ packages/monopoly-common/types/gamePlayerInfo.ts
@@ -1,11 +1,11 @@
-import { PlayerInfo } from '@prisel/common';
+import { player_info } from '@prisel/protos';
 import { Coordinate } from './coordinate';
 /**
  * Information about a game character that player controls
  */
 export interface GamePlayerInfo {
     money: number;
-    player: PlayerInfo;
+    player: player_info.PlayerInfo;
     pos: Coordinate;
     // character determine the sprite set, or color client side use to
     // denote the player

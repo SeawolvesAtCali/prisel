6f782d90dad9af23cc77134d0bc4edb06514a267
Yiou Chen <chen.yiou1992@gmail.com>
2021-01-09 10:05:38 -0800

    remove Ref.ts

--- packages/monopoly-common/world/Ref.ts
+++
@@ -1,7 +0,0 @@
-import { GameObject } from './GameObject';
-import { Id } from './Id';
-export const RefIdSymbol = Symbol('refId');
-export interface Ref<T extends GameObject> {
-    (): T | null;
-    [RefIdSymbol]: Id<T>;
-}

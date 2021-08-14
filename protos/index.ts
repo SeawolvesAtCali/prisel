4347823aa9503dcbcd5e11bab362e173b2900888
Yiou Chen <chen.yiou1992@gmail.com>
2021-08-13 23:08:56 -0700

    build protos/

--- protos/index.ts
+++ protos/index.ts
@@ -1,4 +1,3 @@
 export * as monopolypb from './gen_ts/monopoly/index';
 export * as priselpb from './gen_ts/prisel/index';
-export * as tic_tac_toepb from './gen_ts/tic_tac_toe/index';
-export * as protobuf from './src/protobuf';
+export * as tic_tac_toepb from './gen_ts/tic-tac-toe/index';

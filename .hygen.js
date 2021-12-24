3460bc7a647b7ac2fb2d47aef073e25119bf95ba
Yiou Chen <chen.yiou1992@gmail.com>
2021-12-24 15:42:04 -0800

    removed monopoly-client. upgraded rush

--- .hygen.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    templates: path.resolve(__dirname, '_templates'),
-    helpers: {},
-};
--- packages/monopoly-client/assets/Script/SpriteFrameEntry.ts
+++
@@ -1,9 +0,0 @@
-const { ccclass, property } = cc._decorator;
-
-@ccclass('SpriteFrameEntry')
-export class SpriteFrameEntry {
-    @property(cc.String)
-    public name: string = '';
-    @property(cc.SpriteFrame)
-    public sprite?: cc.SpriteFrame = undefined;
-}
